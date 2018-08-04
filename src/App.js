3fd68fcaf262ddbbce5685d85ab77e74214405ee
hi_captain_ <7273captain@gmail.com>
2018-08-04 19:35:43 +0900

    Make Calendar

--- src/App.js
+++ src/App.js
@@ -21,7 +21,12 @@ class App extends Component {
   componentWillMount(){
     this._get_month()
   }
-
+  componentDidMount(){
+    this._find_today()
+  }
+  componentDidUpdate(){
+    this._find_today()
+  }
   render() {
     return (
       <div className="App">
@@ -35,7 +40,9 @@ class App extends Component {
         </header>
         <section>
           {this.state.thisDates.map((value, i) => {
-            return <span key={i}>{value}</span>
+            return (
+              value.date === 1 ? <span className="dates" data-fulldate={value.fulldate} data-month={value.month} key={i}>{value.month}월 {value.date}일</span> : <span className="dates" data-fulldate={value.fulldate} data-month={value.month} key={i}>{value.date}일</span>
+            )
           })}
         </section>
         <div>
@@ -44,11 +51,33 @@ class App extends Component {
       </div>
     );
   }
+
   _get_month = () => {
-    const last = new Date(today.getFullYear(), today.getMonth() + 1, 0)
-    const dates = []
+    const first = new Date(year, month -1 , 1),
+          last = new Date(year, month, 0),
+          dates = [],
+          first_day = first.getDay(),
+          last_day = last.getDay()
+    for(var be = first_day; be > 0; be--){
+      const be_newDate = new Date(year, month -1, 1 - be),
+            be_month = be_newDate.getMonth() + 1,
+            be_date = be_newDate.getDate(),
+            be_fulldate = be_newDate.toLocaleDateString()
+      dates.push({month : be_month, date : be_date, fulldate : be_fulldate})
+    }
     for(var i = 1; i <= last.getDate(); i++){
-      i === 1 ? dates.push(month + '월' + i + '일') : dates.push(i + '일')
+      const this_newDate = new Date(year, month -1 , i),
+            this_month = this_newDate.getMonth() + 1,
+            this_date = this_newDate.getDate(),
+            this_fulldate = this_newDate.toLocaleDateString()
+      dates.push({month : this_month, date : this_date, fulldate : this_fulldate})
+    }
+    for(var af = 1; af < 7 - last_day; af++){
+      const af_newDate = new Date(year, month, af),
+            af_month = af_newDate.getMonth() + 1,
+            af_date = af_newDate.getDate(),
+            af_fulldate = af_newDate.toLocaleDateString()
+      dates.push({month : af_month, date : af_date, fulldate : af_fulldate})
     }
     this.setState({
       thisYear : year,
@@ -76,6 +105,17 @@ class App extends Component {
     month = today.getMonth() + 1
     year = today.getFullYear()
     this._get_month()
+  }
+
+  _find_today = () => {
+    const getToday = new Date().toLocaleDateString(),
+          list = document.querySelectorAll('.dates')
+    for(var i = 1; i < list.length; i++){
+      list[i].classList.remove('today')
+      if(list[i].dataset.fulldate === getToday){
+        list[i].classList.add('today')
+      }
+    }
   }
 }
 
